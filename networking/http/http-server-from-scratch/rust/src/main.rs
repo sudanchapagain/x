1cabae33c1adc4ba5fce0cde4874731b6fb84d1e
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2025-05-21 11:31:34 +0545

    initial

--- networking/http/http-server-from-scratch/rust/src/main.rs
+++ networking/http/http-server-from-scratch/rust/src/main.rs
@@ -1,3 +1,29 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello, World!</h1></body></html>";
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    println!("Server running on 127.0.0.1:7878");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        thread::spawn(|| {
+            handle_client(stream);
+        });
+    }
 }
